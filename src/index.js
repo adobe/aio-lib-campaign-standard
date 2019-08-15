a20a765e92d108d67d3af0c410eaf8aeeb256562
Shazron Abdullah <shaz@adobe.com>
2019-08-15 12:38:01 +0800

    feat: support filters, lineCount, order, and descendingSort in getAllServices

--- src/index.js
+++ src/index.js
@@ -199,10 +199,20 @@ class CampaignStandardCoreAPI {
 
   /**
    * Get all Service records
+   *
+   * @param {Array} filters apply the filters to the results. List of filters for a resource can be retrieved via a getMetadataForResource call
+   * @param {Object} param parameters to pass
+   * @param {integer} param.lineCount limit the number of records to return (default is 25)
+   * @param {string} param.order the field to order your records by (see the fields of a {@link https://docs.campaign.adobe.com/doc/standard/en/api/ACS_API.html#service|Service})
+   * @param {descendingSort} param.descendingSort set to true to get results in descending order (default is ascending)
+   *
+   * @see getMetadataForResource
    */
-  getAllServices () {
+  getAllServices (filters, { lineCount, order, descendingSort } = {}) {
+    const params = this.__createFilterParams(filters, lineCount, order, descendingSort)
+
     return new Promise((resolve, reject) => {
-      this.sdk.apis.service.getAllServices({}, this.__createRequestOptions())
+      this.sdk.apis.service.getAllServices(params, this.__createRequestOptions())
         .then(response => {
           resolve(response)
         })
