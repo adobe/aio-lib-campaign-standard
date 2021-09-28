0651bfe71731ddfb565d1a2f802b2ce5417fece4
Shazron Abdullah <shazron@gmail.com>
2021-09-28 14:41:32 +0800

    fix: update for new aio-lib-core-networking changes

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -23,13 +23,14 @@ const tenantId = process.env.CAMPAIGN_STANDARD_TENANT_ID
 const apiKey = process.env.CAMPAIGN_STANDARD_API_KEY
 const accessToken = process.env.CAMPAIGN_STANDARD_ACCESS_TOKEN
 const E2E_USE_PROXY = process.env.E2E_USE_PROXY
+const HTTPS_PROXY = process.env.HTTPS_PROXY
 
 let proxyServer
 
 beforeAll(async () => {
   if (E2E_USE_PROXY) {
     proxyServer = await createHttpsProxy()
-    process.env.AIO_PROXY_URL = proxyServer.url
+    console.log(`Using test proxy at ${proxyServer.url}`)
   }
 
   sdkClient = await sdk.init(tenantId, apiKey, accessToken)
@@ -39,7 +40,16 @@ afterAll(() => {
   if (proxyServer) {
     proxyServer.stop()
   }
-  delete process.env.AIO_PROXY_URL
+})
+
+// eslint-disable-next-line jest/expect-expect
+test('HTTPS_PROXY must be set if E2E_USE_PROXY is set', () => {
+  // jest wraps process.env, so libraries will not pick up an env change via code change, so it has to be set on the shell level
+  if (E2E_USE_PROXY) {
+    if (!HTTPS_PROXY) {
+      throw new Error(`If you set E2E_USE_PROXY, you must set the HTTPS_PROXY environment variable. Please set it to HTTPS_PROXY=${proxyServer.url}.`)
+    }
+  }
 })
 
 test('sdk init test', async () => {
